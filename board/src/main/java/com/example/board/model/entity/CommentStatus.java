a80e6050cd5e071ec0c0cf92bfc42a04a77747de
digital9houl <117378831+gyuhyuk-hwang@users.noreply.github.com>
2024-08-22 23:34:50 +0900

    Add files via upload

---
+++ board/src/main/java/com/example/board/model/entity/CommentStatus.java
@@ -0,0 +1,5 @@
+package com.example.board.model.entity;
+
+public enum CommentStatus {
+    ACTIVE, DELETE
+}
---
+++ board/src/main/java/com/example/board/repository/CommentRepositoryCustom.java
@@ -0,0 +1,21 @@
+package com.example.board.repository;
+
+import com.example.board.model.entity.Comment;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+
+    private static final QComment comment = QComment.comment;
+    public Comment find(Long commentNo, Long boardNo) {
+        return queryFactory.select(comment)
+                .from(comment)
+                .where(comment.commentNo.eq(commentNo))
+                .where(QBoard.board.boardNo.eq(boardNo))
+                .fetchOne();
+    }
+}
